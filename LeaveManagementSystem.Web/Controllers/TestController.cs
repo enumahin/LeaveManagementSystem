10d52204a28fa6511bfc35a22c0b2ef7deb8fad9
enuma <enuma@DESKTOP-B0KP5HK>
2025-01-22 19:28:23 +0000

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            TestViewModel client = new TestViewModel {
+                Name = "Iyke Melody",
+                DateOfBirth = new DateTime(1984, 10, 09)
+            };
+            return View(client);
+        }
+    }
+}
